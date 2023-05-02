9de8d7a6fc38fb402ce757120f2a78a478699835
Muhammad Towfique Imam <mithu1408@gmail.com>
2023-05-02 16:56:34 +0600

    updated

--- m1/m1app.py
+++ m1/m1app.py
@@ -1,16 +1,20 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter.font import BOLD
-  
-HEADING_FONT = ("Arial", 35, BOLD)
+from datetime import date
+from tkcalendar import Calendar
   
 class M1App(tk.Tk):
      
     # __init__ function for class tkinterApp
     def __init__(self, *args, **kwargs):
-         
         # __init__ function for class Tk
         tk.Tk.__init__(self, *args, **kwargs)
+
+        style = ttk.Style(self)
+        style.configure('heading.TLabel', font=(None, 35, BOLD))
+        style.configure('form.TLabel', font=(None, 11))
+        style.configure('form.TButton', font=(None, 11))
 
         self.title("Matrix Clock")
         self.geometry("600x400")
@@ -47,8 +51,6 @@ class M1App(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise()
   
-# first window frame startpage
-  
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -59,68 +61,60 @@ class StartPage(tk.Frame):
         self.rowconfigure(2, weight=1)
         self.rowconfigure(3, weight=18)
 
-        lbl_heading = ttk.Label(self, text="Matrix Clock M1", font=HEADING_FONT)
+        lbl_heading = ttk.Label(self, text="Matrix Clock M1", style="heading.TLabel")
         lbl_heading.grid(row=0, column=0)
 
-        bn_btn = ttk.Button(self, text="Bangla Setup", width=25, command=lambda: controller.show_frame(BanglaSetupPage))
+        bn_btn = ttk.Button(self, text="Bangla Setup", style="form.TButton", width=25, command=lambda: controller.show_frame(BanglaSetupPage))
         bn_btn.grid(row=1, column=0, padx=20, pady=5)
 
-        hz_btn = ttk.Button(self, text="Hijri Setup", width=25, command=lambda: controller.show_frame(HijriSetupPage))
+        hz_btn = ttk.Button(self, text="Hijri Setup", style="form.TButton", width=25, command=lambda: controller.show_frame(HijriSetupPage))
         hz_btn.grid(row=2, column=0, padx=20, pady=5)
   
-  
-# second window frame page1
 class BanglaSetupPage(tk.Frame):
-     
     def __init__(self, parent, controller):
-         
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text ="Page 1", font = HEADING_FONT)
-        label.grid(row = 0, column = 4, padx = 10, pady = 10)
-  
-        # button to show frame 2 with text
-        # layout2
-        button1 = ttk.Button(self, text ="StartPage",
-                            command = lambda : controller.show_frame(StartPage))
-     
-        # putting the button in its place
-        # by using grid
-        button1.grid(row = 1, column = 1, padx = 10, pady = 10)
-  
-        # button to show frame 2 with text
-        # layout2
-        button2 = ttk.Button(self, text ="Page 2",
-                            command = lambda : controller.show_frame(HijriSetupPage))
-     
-        # putting the button in its place by
-        # using grid
-        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
-  
-  
-  
-  
-# third window frame page2
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=18)
+        self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=1)
+        self.rowconfigure(3, weight=18)
+
+        lbl_heading = ttk.Label(self, text="Bangla Setup", style="heading.TLabel")
+        lbl_heading.grid(row=0, column=0)
+
+        frame = ttk.Frame(self)
+
+        lbl_year = ttk.Label(frame, text="Bangla Year", style="form.TLabel")
+        lbl_year.pack(side=tk.LEFT, padx=0)
+
+        current_year = date.today().year
+        cmb_bn_year = ttk.Combobox(frame, font=(None, 11), values=list(range(current_year-2, current_year+3)))
+        cmb_bn_year.current(2)
+        cmb_bn_year.pack(side=tk.LEFT, padx=10)
+
+        bn_apply = ttk.Button(frame, text="Apply", command=lambda: controller.show_frame(StartPage), style="form.TButton")
+        bn_apply.pack(side=tk.LEFT, padx=10)
+
+        frame.grid(row=1, column=0)
+
 class HijriSetupPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text ="Page 2", font = HEADING_FONT)
-        label.grid(row = 0, column = 4, padx = 10, pady = 10)
-  
-        # button to show frame 2 with text
-        # layout2
-        button1 = ttk.Button(self, text ="Page 1",
-                            command = lambda : controller.show_frame(BanglaSetupPage))
-     
-        # putting the button in its place by
-        # using grid
-        button1.grid(row = 1, column = 1, padx = 10, pady = 10)
-  
-        # button to show frame 3 with text
-        # layout3
-        button2 = ttk.Button(self, text ="Startpage",
-                            command = lambda : controller.show_frame(StartPage))
-     
-        # putting the button in its place by
-        # using grid
-        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=18)
+        self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=1)
+        self.rowconfigure(3, weight=18)
+
+        lbl_heading = ttk.Label(self, text="Hijri Setup", style="heading.TLabel")
+        lbl_heading.grid(row=0, column=0)
+
+        frame = ttk.Frame(self)
+
+        cal = Calendar(frame, selectmode = 'day', year = 2020, month = 5, day = 22)
+        cal.pack(pady = 20)
+
+        frame.grid(row=1, column=0)
   
