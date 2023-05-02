9819f30a004e6d2aad01b793e422db5b7aa27556
Muhammad Towfique Imam <mithu1408@gmail.com>
2023-05-02 15:46:16 +0600

    updated

--- m1/m1app.py
+++ m1/m1app.py
@@ -1,23 +1,126 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
-from tkinter.font import BOLD, Font
-
-from tkcalendar import Calendar
-
-
-class M1App:
-    def __init__(self, master) -> None:
-        self.master = master
+from tkinter.font import BOLD
   
-        self.master.columnconfigure(0, weight=1)
-        self.master.rowconfigure(0, weight=18)
-        self.master.rowconfigure(1, weight=1)
-        self.master.rowconfigure(2, weight=1)
-        self.master.rowconfigure(3, weight=18)
+HEADING_FONT = ("Arial", 35, BOLD)
+  
+class M1App(tk.Tk):
+     
+    # __init__ function for class tkinterApp
+    def __init__(self, *args, **kwargs):
+         
+        # __init__ function for class Tk
+        tk.Tk.__init__(self, *args, **kwargs)
 
-        lbl_heading = Label(text="Matrix Clock M1", font=Font(size=25, weight=BOLD), fg="Grey")
+        self.title("Matrix Clock")
+        self.geometry("600x400")
+        self.resizable(0, 0)
+         
+        # creating a container
+        container = tk.Frame(self) 
+        container.pack(side = "top", fill = "both", expand = True)
+  
+        container.grid_rowconfigure(0, weight = 1)
+        container.grid_columnconfigure(0, weight = 1)
+  
+        # initializing frames to an empty array
+        self.frames = {} 
+  
+        # iterating through a tuple consisting
+        # of the different page layouts
+        for F in (StartPage, BanglaSetupPage, HijriSetupPage):
+  
+            frame = F(container, self)
+  
+            # initializing frame of that object from
+            # startpage, page1, page2 respectively with
+            # for loop
+            self.frames[F] = frame
+  
+            frame.grid(row = 0, column = 0, sticky ="nsew")
+  
+        self.show_frame(StartPage)
+  
+    # to display the current frame passed as
+    # parameter
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+  
+# first window frame startpage
+  
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=18)
+        self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=1)
+        self.rowconfigure(3, weight=18)
+
+        lbl_heading = ttk.Label(self, text="Matrix Clock M1", font=HEADING_FONT)
         lbl_heading.grid(row=0, column=0)
-        bn_btn = Button(text="Bangla", width=25, height=2)
+
+        bn_btn = ttk.Button(self, text="Bangla Setup", width=25, command=lambda: controller.show_frame(BanglaSetupPage))
         bn_btn.grid(row=1, column=0, padx=20, pady=5)
-        hz_btn = Button(text="Hijri", width=25, height=2)
+
+        hz_btn = ttk.Button(self, text="Hijri Setup", width=25, command=lambda: controller.show_frame(HijriSetupPage))
         hz_btn.grid(row=2, column=0, padx=20, pady=5)
+  
+  
+# second window frame page1
+class BanglaSetupPage(tk.Frame):
+     
+    def __init__(self, parent, controller):
+         
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text ="Page 1", font = HEADING_FONT)
+        label.grid(row = 0, column = 4, padx = 10, pady = 10)
+  
+        # button to show frame 2 with text
+        # layout2
+        button1 = ttk.Button(self, text ="StartPage",
+                            command = lambda : controller.show_frame(StartPage))
+     
+        # putting the button in its place
+        # by using grid
+        button1.grid(row = 1, column = 1, padx = 10, pady = 10)
+  
+        # button to show frame 2 with text
+        # layout2
+        button2 = ttk.Button(self, text ="Page 2",
+                            command = lambda : controller.show_frame(HijriSetupPage))
+     
+        # putting the button in its place by
+        # using grid
+        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
+  
+  
+  
+  
+# third window frame page2
+class HijriSetupPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text ="Page 2", font = HEADING_FONT)
+        label.grid(row = 0, column = 4, padx = 10, pady = 10)
+  
+        # button to show frame 2 with text
+        # layout2
+        button1 = ttk.Button(self, text ="Page 1",
+                            command = lambda : controller.show_frame(BanglaSetupPage))
+     
+        # putting the button in its place by
+        # using grid
+        button1.grid(row = 1, column = 1, padx = 10, pady = 10)
+  
+        # button to show frame 3 with text
+        # layout3
+        button2 = ttk.Button(self, text ="Startpage",
+                            command = lambda : controller.show_frame(StartPage))
+     
+        # putting the button in its place by
+        # using grid
+        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
+  
